3444dc91c6d0787fc95b09d0ff49cdea22a6df0e
Enrique Lopez <llenriquelopez@gmail.com>
2020-11-07 21:36:51 -0600

    Layout for initial page

--- src/lib/intermoto_web/controllers/initial_controller.ex
+++ src/lib/intermoto_web/controllers/initial_controller.ex
@@ -1,6 +1,8 @@
 defmodule IntermotoWeb.InitialController do
   use IntermotoWeb, :controller
 
+  plug :put_layout, "initial.html"
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
